a602e74e2235220a8669ddf3d72d5212fe15835e
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-31 19:22:15 +0530

    Create ReverseIgnoreEven.java

---
+++ ReverseIgnoreEven.java
@@ -0,0 +1,16 @@
+public class ReverseIgnoreEven {
+
+    public static int reverseIgnoringEvenDigits(int number) {
+        int reversed = 0;
+        // Fill Out the Logic Here to print reversed number by ignoring even digits in a number
+      // Example 1432 
+      // print 31 as an Output by ignoring Even Digits and Reversing it
+      return reversed;
+    }
+
+    public static void main(String[] args) {
+        int number = 123456;
+        int result = reverseIgnoringEvenDigits(number);
+        System.out.println("Reversed number excluding even digits: " + result);
+    }
+}
