fc1f2d21cb464853285ede90b5fc066c02626362
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-31 19:29:40 +0530

    Create SecondLargest.java

---
+++ SecondLargest.java
@@ -0,0 +1,16 @@
+public class SecondLargest {
+
+    public static int findSecondLargestDigit(int number) {
+        int largest = -1;
+        int secondLargest = -1;
+       // prepare a Logic to findout second largest digit of the number
+    }
+        return secondLargest;
+    }
+
+    public static void main(String[] args) {
+        int number = 123456;
+        int secondLargest = findSecondLargestDigit(number);
+        System.out.println("Second largest digit: " + secondLargest);
+    }
+}
