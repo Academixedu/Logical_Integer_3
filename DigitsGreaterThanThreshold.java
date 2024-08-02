9b53f151e9f370b7b7d2aa6196dbee9e320a569d
Pavani23678 <pavaniburadhakavi@gmail.com>
2024-08-02 14:12:26 +0530

    integer3

--- DigitsGreaterThanThreshold.java
+++ DigitsGreaterThanThreshold.java
@@ -3,6 +3,13 @@ public class DigitsGreaterThanThreshold {
     public static int countDigitsGreaterThan(int number, int threshold) {
         int count = 0;
         //prepare the Logic to find out count of numbers which is greater than threshold 
+        while(number!=0){
+            int b=number%10;
+            if(b>threshold){
+                count++;
+            }
+            number=number/10;
+        }
       return count;
     }
 
--- SecondLargest.java
+++ SecondLargest.java
@@ -4,9 +4,22 @@ public class SecondLargest {
         int largest = -1;
         int secondLargest = -1;
        // prepare a Logic to findout second largest digit of the number
-    }
+       while(number>0){
+        int b=number%10;
+        number=number/10;
+        if(b>largest){
+            secondLargest=largest;
+            largest=b;
+        }
+        else if(b>secondLargest && b!=largest){
+            secondLargest=b;
+        }
+       }
+       
+
+    
         return secondLargest;
-    }
+}
 
     public static void main(String[] args) {
         int number = 123456;
