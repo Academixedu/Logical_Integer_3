334e907855189bdaf86618ead39cc29057ff9c2d
Manisugguna0 <subrahmanyamanikanta@gmail.com>
2024-08-02 16:39:33 +0530

    MANI

--- DivisorDigitCount.java
+++ DivisorDigitCount.java
@@ -6,6 +6,20 @@ public class DivisorDigitCount {
         // Problem: Write a Logic to count the number of digits in a number that are also divisors of the number.
         // Example: For the number 128, the digits 1, 2, and 8 are all divisors of 128. The count should be 3.
       // if we take 28 only 2 is the factor of the digits hence it should return count as 1
+    
+        while(number > 0)
+      {
+        int rem = number % 10;
+        for(int i = 1;i<=number;i++)
+        {
+            if(number % i==0)
+            {
+                System.out.println(i);
+            }
+        }
+        number = number / 10;
+        count++;
+      }
         return count;
     }
 
