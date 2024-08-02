0636fb37b6c2abed5677cfabcf5a0333850995fc
Bharathmanne18 <bharathmanne12@gmail.com>
2024-08-02 18:05:20 +0530

    Update SecondLargest.java

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,10 +1,21 @@
+ 
 public class SecondLargest {
 
     public static int findSecondLargestDigit(int number) {
         int largest = -1;
         int secondLargest = -1;
        // prepare a Logic to findout second largest digit of the number
-    }
+       while(number>0){
+        int b=number%10;
+        number=number/10;
+        if(b>largest){
+            secondLargest=largest;
+            largest=b;
+        }
+        else if(b>secondLargest && b!=largest){
+            secondLargest=b;
+        }
+       }
         return secondLargest;
     }
 
