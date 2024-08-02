bde14cff493cf67500af428af33fe96ce1e57358
Shreyash000007 <shreyasharg@gmail.com>
2024-08-02 15:30:59 +0530

    Shreyash

--- DigitsGreaterThanThreshold.java
+++ DigitsGreaterThanThreshold.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
+
 public class DigitsGreaterThanThreshold {
 
     public static int countDigitsGreaterThan(int number, int threshold) {
         int count = 0;
         //prepare the Logic to find out count of numbers which is greater than threshold 
+        Scanner in=new Scanner(System.in);
+
+        System.out.println(" ");
+        System.out.print("Enter your Number = ");
+        int a=in.nextInt();
+        while (a!=0) 
+        {
+            int rem=a%10;
+            if(rem>threshold)
+            {
+                
+                count++;
+            }
+            a=a/10;
+            in.close();
+        }
+
+
       return count;
     }
 
--- SecondLargest.java
+++ SecondLargest.java
@@ -1,11 +1,33 @@
 public class SecondLargest {
 
-    public static int findSecondLargestDigit(int number) {
-        int largest = -1;
-        int secondLargest = -1;
-       // prepare a Logic to findout second largest digit of the number
-    }
-        return secondLargest;
+    public static int findSecondLargestDigit(int number) 
+    {
+        int largest = 0;
+        int secondLargest = 0;
+        // prepare a Logic to findout second largest digit of the number
+
+
+        //int rem=number%10;
+        // secondLargest=rem;
+        while(number!=0)
+        {
+            int rem=number%10;
+            // number=number/10;
+
+            if(largest<rem)
+            {
+                secondLargest=largest;
+                largest=rem;
+            }
+            else if(rem<largest && rem>secondLargest)
+            {
+                secondLargest=rem;
+            }
+            number=number/10;
+
+       }
+
+       return secondLargest;
     }
 
     public static void main(String[] args) {
