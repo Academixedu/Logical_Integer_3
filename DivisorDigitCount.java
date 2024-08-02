395c29b0dde04bcfb240ff11b6be2102b7a735d4
2110Abhishek <agchoudhari2110@gmail.com>
2024-08-02 23:28:56 +0530

    Abhishek

--- DivisorDigitCount.java
+++ DivisorDigitCount.java
@@ -1,17 +1,31 @@
-public class DivisorDigitCount {
+public static int countDivisorDigits(int number) 
+{
+    int count = 0;
+    int temp = number;
+    // Problem: Write a Logic to count the number of digits in a number that are also divisors of the number.
+    // Example: For the number 128, the digits 1, 2, and 8 are all divisors of 128. The count should be 3.
+  // if we take 28 only 2 is the factor of the digits hence it should return count as 1
 
-    public static int countDivisorDigits(int number) {
-        int count = 0;
-        int temp = number;
-        // Problem: Write a Logic to count the number of digits in a number that are also divisors of the number.
-        // Example: For the number 128, the digits 1, 2, and 8 are all divisors of 128. The count should be 3.
-      // if we take 28 only 2 is the factor of the digits hence it should return count as 1
-        return count;
+  while(temp!=0)
+  {
+    int div=temp%10;
+    if(number%div==0)
+    {
+        count++;
     }
+    temp/=10;
+    //temp=temp/10
+  }
 
-    public static void main(String[] args) {
-        int number = 128;
-        int count = countDivisorDigits(number);
-        System.out.println("Count of divisor digits: " + count);
-    }
+    return count;
+}
+
+public static void main(String[] args) {
+    int number = 128;
+public static void main(String[] args) 
+{
+    int number = 124;
+    int count = countDivisorDigits(number);
+    System.out.println("Count of divisor digits: " + count);
+}
 }
--- SecondLargest.java
+++ SecondLargest.java
@@ -1,4 +1,5 @@
-public class SecondLargest {
+public class SecondLargest 
+{
 
     public static int findSecondLargestDigit(int number) {
         int largest = -1;
@@ -6,10 +7,42 @@ public class SecondLargest {
        // prepare a Logic to findout second largest digit of the number
     }
         return secondLargest;
+    public static int findSecondLargestDigit(int number) 
+    {
+        int largest = 0;
+        int secondLargest = 0;
+        // prepare a Logic to findout second largest digit of the number
+
+
+        //int rem=number%10;
+        // secondLargest=rem;
+        while(number!=0)
+        {
+            int rem=number%10;
+            // number=number/10;
+
+            if(largest<rem)
+            {
+                secondLargest=largest;
+                largest=rem;
+            }
+            else if(rem<largest && rem>secondLargest)
+            {
+                secondLargest=rem;
+            }
+            number=number/10;
+
+       }
+
+       return secondLargest;
     }
+
 
     public static void main(String[] args) {
         int number = 123456;
+    public static void main(String[] args) 
+    {
+        int number = 8924;
         int secondLargest = findSecondLargestDigit(number);
         System.out.println("Second largest digit: " + secondLargest);
     }
