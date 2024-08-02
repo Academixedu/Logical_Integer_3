a3d8aef0698822cf9fc1a276755872c24b437d36
fayazshaik515 <fayazshaik231104@gmail.com>
2024-08-02 16:54:53 +0530

    from fayaz

--- DigitsGreaterThanThreshold.java
+++ DigitsGreaterThanThreshold.java
@@ -3,6 +3,14 @@ public class DigitsGreaterThanThreshold {
     public static int countDigitsGreaterThan(int number, int threshold) {
         int count = 0;
         //prepare the Logic to find out count of numbers which is greater than threshold 
+        while(number >0){
+            int dig = number%10;
+            if(dig>threshold){
+                count++;
+            }
+            number/=10;
+            
+        }
       return count;
     }
 
@@ -11,5 +19,6 @@ public class DigitsGreaterThanThreshold {
         int threshold = 3;
         int count = countDigitsGreaterThan(number, threshold);
         System.out.println("Count of digits greater than " + threshold + ": " + count);
+        DigitsGreaterThanThreshold a = new DigitsGreaterThanThreshold();
     }
 }
--- ReverseIgnoreEven.java
+++ ReverseIgnoreEven.java
@@ -5,6 +5,15 @@ public class ReverseIgnoreEven {
         // Fill Out the Logic Here to print reversed number by ignoring even digits in a number
       // Example 1432 
       // print 31 as an Output by ignoring Even Digits and Reversing it
+      while(number>0){
+        int dig = number%10;//5
+        if(dig%2!=0){
+            reversed=reversed*10+dig;
+            
+        }
+        number=number/10;
+      }
+
       return reversed;
     }
 
