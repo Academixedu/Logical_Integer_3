902c28cb91513117f7d4e92b77bcd7f9aa23c348
nayeem6836 <maseednayeem@gmail.com>
2024-08-03 09:36:40 +0530

    Update DigitsGreaterThanThreshold.java

--- DigitsGreaterThanThreshold.java
+++ DigitsGreaterThanThreshold.java
@@ -1,15 +1,13 @@
-public class DigitsGreaterThanThreshold {
-
-    public static int countDigitsGreaterThan(int number, int threshold) {
+public static int countDigitsGreaterThan(int number, int threshold) {
         int count = 0;
         //prepare the Logic to find out count of numbers which is greater than threshold 
+        while(number >0){
+            int dig = number%10;
+            if(dig>threshold){
+                count++;
+            }
+            number/=10;
+
+        }
       return count;
     }
-
-    public static void main(String[] args) {
-        int number = 123456;
-        int threshold = 3;
-        int count = countDigitsGreaterThan(number, threshold);
-        System.out.println("Count of digits greater than " + threshold + ": " + count);
-    }
-}
